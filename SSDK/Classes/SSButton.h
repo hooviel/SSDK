70b28e7712f74d00584c4075033a0c279669148f
David <hooviel@qq.com>
2018-07-17 18:58:35 +0800

    修改名称

---
+++ SSDK/Classes/SSButton.h
@@ -0,0 +1,14 @@
+//
+//  SSButton.h
+//  Pods-SSDK_Example
+//
+//  Created by David on 2018/7/17.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSButton : NSObject
+
+- (void)say;
+
+@end
---
+++ SSDK/Classes/SSButton.m
@@ -0,0 +1,16 @@
+//
+//  SSButton.m
+//  Pods-SSDK_Example
+//
+//  Created by David on 2018/7/17.
+//
+
+#import "SSButton.h"
+
+@implementation SSButton
+
+- (void)say {
+    NSLog(@"Hello world~~~~~😄🐶");
+}
+
+@end
